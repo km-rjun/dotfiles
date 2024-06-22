f6dedbad4f492b9df79d52c9cc482ff77db8c153
km-rjun <rjun@Morpheus.localdomain>
2024-06-22 15:29:58 +0530

    Minor changes to nvim plugins

---
+++ .config/nvim/lua/core/plugin_config/bufferline.lua
@@ -0,0 +1,23 @@
+require("bufferline").setup{
+    options = {
+    offsets = {
+        {
+            filetype = "NvimTree",
+            text = function()
+                return vim.fn.getcwd()
+            end,
+            highlight = "Directory",
+            text_align = "left"
+        }
+    },
+    diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and " " or ""
+        return " " .. icon .. count
+    end,
+        hover = {
+            enabled = true,
+            delay = 200,
+            reveal = {'close'}
+        }
+    },
+}
