7bf065750b2b3487570d12fbd4aec97d46d0d282
km-rjun <rjun@Morpheus.localdomain>
2024-08-05 00:48:23 +0530

    LSP improvements

--- nvim/.config/nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/.config/nvim/lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "ruby", "vim", "cpp", "css",  },
+  ensure_installed = { "c", "lua", "rust", "java", "vim", "cpp", "css" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
