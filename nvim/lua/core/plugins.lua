e9c70f4666745767f29dd48d8efcaebbe7ffeb50
km-rjun <mail.rjun.km@gmail.com>
2025-06-29 21:28:25 +0530

    Added lazygit plugin

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -25,6 +25,28 @@ local plugins = {
     'akinsho/bufferline.nvim',
     'lewis6991/gitsigns.nvim',
 
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        },
+                config = function()
+                require("telescope").load_extension("lazygit")
+            end,
+},
+
     -- LSP support
     'neovim/nvim-lspconfig',
     'williamboman/mason.nvim',
