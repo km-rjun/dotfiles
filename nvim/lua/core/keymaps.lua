9ecb673a9e3688238a1257e6c44bb8817fd50647
km-rjun <mail.rjun.km@gmail.com>
2026-03-28 23:32:58 +0530

    fix clipboard neovim integration

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,18 +1,17 @@
 local opts = { noremap = true, silent = true }
 
--- vim.keymap.set('n', '<leader>t', ':nohlsearch<CR>', opts)
+vim.keymap.set("n", "<leader>t", ":nohlsearch<CR>", opts)
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", '"+y', opts)
+vim.keymap.set("v", "<leader>y", '"+y', opts)
+vim.keymap.set("n", "<leader>Y", '"+Y', opts)
+vim.keymap.set("x", "<leader>p", '"_dP', opts)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
-vim.keymap.set("x", "<leader>p", "\"_dP")
 
 vim.keymap.set("n", "<C,up>", ":resize +2<CR>", opts)
 vim.keymap.set("n", "<C,down>", ":resize -2<CR>", opts)
