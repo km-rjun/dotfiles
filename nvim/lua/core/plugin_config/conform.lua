1a6bf768e09c9a46243e93028f0dacec3a5f5bfe
km-rjun <mail.rjun.km@gmail.com>
2026-05-03 00:49:17 +0530

    remove fomat on save

--- nvim/lua/core/plugin_config/conform.lua
+++ nvim/lua/core/plugin_config/conform.lua
@@ -13,13 +13,8 @@ require("conform").setup({
         hcl        = { "terraform_fmt" },
         go         = { "gofmt" },
     },
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,   -- fall back to LSP format if no formatter configured
-    },
 })
 
--- Manual format keymap (overrides lsp buf.format)
 vim.keymap.set({ "n", "v" }, "<leader>cf", function()
     require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format buffer/selection" })
