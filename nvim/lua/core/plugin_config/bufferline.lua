0f0a77080d96860df4310ffacd16184f577a6eef
km-rjun <mail.rjun.km@gmail.com>
2026-05-02 23:39:59 +0530

    added customizations

--- nvim/lua/core/plugin_config/bufferline.lua
+++ nvim/lua/core/plugin_config/bufferline.lua
@@ -1,25 +1,30 @@
 require("bufferline").setup({
-    options = {
-        mode              = "buffers",
-        separator_style   = "slant",
-        always_show_bufferline = true,
-        show_buffer_close_icons = true,
-        show_close_icon   = false,
-        color_icons       = true,
-        diagnostics       = "nvim_lsp",
-        diagnostics_indicator = function(_, _, diag)
-            local icons = { error = " ", warning = " " }
-            local ret = (diag.error and icons.error .. diag.error .. " " or "")
-                     .. (diag.warning and icons.warning .. diag.warning or "")
-            return vim.trim(ret)
-        end,
-        offsets = {
-            {
-                filetype   = "neo-tree",
-                text       = "File Explorer",
-                highlight  = "Directory",
-                separator  = true,
-            },
-        },
-    },
+	options = {
+		mode = "buffers",
+		separator_style = "thin",
+		always_show_bufferline = true,
+		show_buffer_close_icons = true,
+		show_close_icon = false,
+		color_icons = true,
+		diagnostics = "nvim_lsp",
+		diagnostics_indicator = function(_, _, diag)
+			local icons = { error = " ", warning = " " }
+			local ret = (diag.error and icons.error .. diag.error .. " " or "")
+				.. (diag.warning and icons.warning .. diag.warning or "")
+			return vim.trim(ret)
+		end,
+		hover = {
+			enabled = true,
+			delay = 200,
+			reveal = { "close" },
+		},
+		offsets = {
+			{
+				filetype = "neo-tree",
+				text = "File Explorer",
+				highlight = "Directory",
+				separator = true,
+			},
+		},
+	},
 })
