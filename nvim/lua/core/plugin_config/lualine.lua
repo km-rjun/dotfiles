729e272ee5f920e7a19ed62825348dc333201c12
km-rjun <mail.rjun.km@gmail.com>
2026-05-06 23:11:41 +0530

    edit: separators

--- nvim/lua/core/plugin_config/lualine.lua
+++ nvim/lua/core/plugin_config/lualine.lua
@@ -2,7 +2,7 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "catppuccin-mocha",
-		component_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "/" },
 		section_separators = { left = "", right = "" },
 		globalstatus = true,
 	},
@@ -12,11 +12,11 @@ require("lualine").setup({
 		lualine_c = {
 			{
 				"filename",
-				symbols = {
-					modified = " ●",
-					alternate_file = "#",
-					directory = "",
-				},
+				-- symbols = {
+				-- 	modified = " ●",
+				-- 	alternate_file = "#",
+				-- 	directory = "",
+				-- },
 			},
 		},
 		lualine_x = { "encoding", "fileformat", "filetype" },
